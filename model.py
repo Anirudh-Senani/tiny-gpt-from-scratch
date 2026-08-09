6c99971bdf9eb827a3565840ae879304274de917
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-09 14:48:19 +0530

    Step 75: derive_dx_on_paper

--- model.py
+++ model.py
@@ -595,8 +595,13 @@ def linear_forward(x, w):
         cache=dict(x=x, w=w)
     )
 
-# Step 75 - derive_dx_on_paper (not yet solved)
-# TODO: implement
+# Step 75 - derive_dx_on_paper
+def derive_dx_on_paper():
+    """Return notes deriving dL/dX = dY @ W.T for Y = X @ W."""
+    # TODO: return a multi-line string with the derivation and shape check
+    return """Y = X @ W
+dL/dX = dY @ W.T
+shapes: X (B, In), W (In, Out), dY (B, Out) -> dL/dX (B, In)"""
 
 # Step 76 - derive_linear_dw_on_paper (not yet solved)
 # TODO: implement
