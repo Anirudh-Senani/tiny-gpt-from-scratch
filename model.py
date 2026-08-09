c36c6e3b3ae72fb26b367e785368e8571a78e336
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-09 18:24:05 +0530

    Step 91: layernorm_backward_implementation

--- model.py
+++ model.py
@@ -744,8 +744,10 @@ def layernorm_backward_full(dy, cache):
         dbeta=(dy).sum(axis=0)
     )
 
-# Step 91 - layernorm_backward_implementation (not yet solved)
-# TODO: implement
+# Step 91 - layernorm_backward_implementation
+def layernorm_backward_implementation(d_out, cache):
+    # TODO: return {'dx', 'dgamma', 'dbeta'} gradients for LayerNorm given d_out and the forward cache.
+    return layernorm_backward_full(d_out, cache)
 
 # Step 92 - create_token_embedding (not yet solved)
 # TODO: implement
