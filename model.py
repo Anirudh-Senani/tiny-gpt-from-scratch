2e82bf9bcbecccd41876ec287d8f506b9cf562e2
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-11 21:30:38 +0530

    Step 152: adam_bias_correction

--- model.py
+++ model.py
@@ -1523,8 +1523,11 @@ def adam_update_second_moment(v_prev, grad, beta2):
     # TODO: blend v_prev with the squared gradient using beta2
     return beta2*v_prev + (1-beta2)*(grad**2)
 
-# Step 152 - adam_bias_correction (not yet solved)
-# TODO: implement
+# Step 152 - adam_bias_correction
+def adam_bias_correction(m, v, beta1, beta2, t):
+    """Return bias-corrected (m_hat, v_hat) for Adam at step t."""
+    # TODO: divide m and v by (1 - beta**t) factors to remove init bias
+    return m/(1-(beta1**t)), v/(1-(beta2**t))
 
 # Step 153 - adam_parameter_update (not yet solved)
 # TODO: implement
