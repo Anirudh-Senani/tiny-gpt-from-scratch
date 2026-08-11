d5012e500bebdc86fcb4d3e540212ce4a590d114
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-11 21:27:20 +0530

    Step 148: initialize_adam_step_counter

--- model.py
+++ model.py
@@ -1497,8 +1497,11 @@ def initialize_adam_moments(model_params):
 
     return m,v
 
-# Step 148 - initialize_adam_step_counter (not yet solved)
-# TODO: implement
+# Step 148 - initialize_adam_step_counter
+def initialize_adam_step_counter():
+    """Return the initial Adam step counter t."""
+    # TODO: return the starting value of the Adam time-step counter.
+    return 0
 
 # Step 149 - adam_increment_step (not yet solved)
 # TODO: implement
